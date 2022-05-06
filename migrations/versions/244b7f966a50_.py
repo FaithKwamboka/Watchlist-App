da537dc7dbd0e1e5a5112d1e1832b430d1f997ee
FaithKwamboka <okongofaith3@gmail.com>
2022-05-06 19:05:58 +0300

    Fixing bugs

---
+++ migrations/versions/244b7f966a50_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 244b7f966a50
+Revises: 
+Create Date: 2022-05-06 13:51:52.714719
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '244b7f966a50'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export MOVIE_API_KEY='e5396e534ae1d9a462751e4e8be2e9be'
 export SECRET_KEY=']C/L.VFC?PM$@a/7/}t?[D&+6{jP:Z'
 
-export MAIL_USERNAME='okongofaith3@gmail.com'
-export MAIL_PASSWORD='kwamboka@22'
+export MAIL_USERNAME='faith.okongo@student.moringaschool.com'
+export MAIL_PASSWORD='kwamboka22'
 
 python3.8 manage.py server
